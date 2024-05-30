741a7d6197db1477bcfa9cd57e3fd2eb84e1b73a
Sergey Ovsyannikov <ovssv87@gmail.com>
2024-05-30 22:56:14 +0400

    1

--- Lesson_4/task_1/main.cpp
+++ Lesson_4/task_1/main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 #include <pqxx/pqxx>
 #include <Windows.h>
+#include <string>
 #pragma execution_character_set ("UTF-8")
 
+class Study_DB
+{
+public:
+	Study_DB(std::string& host, std::string& port, std::string& dbname, std::string& user, std::string& password)
+	{
+		std::ostringstream in;
+		in << "host = " << host
+			<< " port = " << port
+			<< " dbname = " << dbname
+			<< " user = " << user
+			<< " password = " << password;
+		c = new pqxx::connection (in.str());
+	};
+	~Study_DB() 
+	{
+		delete c;
+	};
+
+private:
+	pqxx::connection* c;
+};
 
 
 int main()
@@ -13,12 +35,12 @@ int main()
 
 	try
 	{
-	pqxx::connection c(
-		"host=localhost "
-		"port=5432 "
-		"dbname=lesson_DB_4 "
-		"user=Study_DB "
-		"password=mellon");
+		std::string host{ "localhost" };
+		std::string port{ "5432" };
+		std::string dbname{ "lesson_DB_4" };
+		std::string user{ "Study_DB" };
+		std::string password{ "mellon" };
+		Study_DB study(host, port, dbname, user, password);
 
 	std::cout << "Hello" << std::endl;
 
